8c89e6b0158c2be0bae59a5886ffcfb7bb017c5e
kuangsith <108988177+kuangsith@users.noreply.github.com>
2023-04-12 23:21:30 +0700

    Update word list and entropy

--- make_start_ent.py
+++ make_start_ent.py
@@ -1,10 +1,14 @@
 import wordle_help
 import pandas as pd
 
-df = pd.read_csv("start_entropy.csv",index_col=0)
+
 
 with open('low.txt', 'r') as f:
     all = [line.strip() for line in f.readlines()]
+
+zeros = [1.01 for i in all]
+dict = {"Expected entropy": zeros}
+df = pd.DataFrame(dict, index = all)
 
 with open('listofwordlewords.txt','r') as f2:
     wordles_ans = [line.strip() for line in f2.readlines()]
